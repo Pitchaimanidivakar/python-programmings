b5a63239e8eed9ce524a7b4f826b0c64e3b4f77b
divakarmani <36753321+divakarmani@users.noreply.github.com>
2018-02-23 14:47:13 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,7 @@
+string = raw_input ("Enter the string:")
+vowels=0
+for j in string:
+      if(j=='a' or j=='e' or j=='i' or j=='o' or j=='u' or j=='A' or j=='E' or j=='I' or j=='O' or j=='U'):
+            vowels=vowels+1
+print("Number of vowels are:")
+print(vowels)
